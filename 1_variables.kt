0069509a81eead44cf1654d5f4bffdca6b94380e
lintangwisesa <lintangwisesa@ymail.com>
2019-03-29 11:19:44 +0700

    basic kotlin

---
+++ 1_variables.kt
@@ -0,0 +1,10 @@
+// variables
+fun main(args: Array<String>) {
+    var kata = "Tes 1 2 3"
+    var angkaInt = 12
+    var angkaFlo = 1.9
+    
+    println(kata)
+    println(angkaInt)
+    println(angkaFlo)
+}
---
+++ 2_immutableVar.kt
@@ -0,0 +1,14 @@
+// variables
+fun main(args: Array<String>) {
+    
+    // mutable string
+    var kata1: String
+    kata1 = "Tes 1 2 3"
+    kata1 = "Tes 4 5 6"
+    
+    // immutable string
+    val kata2 = "Ini constant, tak bisa diubah!"
+    
+    println(kata1)
+    println(kata2)
+}
